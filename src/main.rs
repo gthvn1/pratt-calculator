729c915b577b5dad9dad610f2bb61662aa50230b
Guillaume <guillaume.thouvenin@vates.tech>
2025-05-16 13:48:23 +0200

    start REPL

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use std::io::Write;
+
 fn main() {
-    println!("Hello, world!");
+    let stdin = std::io::stdin();
+    let mut stdout = std::io::stdout();
+    let mut input = String::new();
+
+    println!("Starting REPL. Ctrl+D to quit");
+
+    loop {
+        print!(">> ");
+        stdout.flush().unwrap();
+        input.clear();
+
+        match stdin.read_line(&mut input) {
+            Ok(0) => break,
+            Ok(n) => {
+                println!("..ECHO {} bytes <{}>", n, &input);
+            }
+            Err(e) => eprintln!("failed to read line: {}", e),
+        }
+    }
 }
